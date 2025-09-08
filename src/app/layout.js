0d254d93691f265eee169c939c80577f0eec04ea
SanellmanL <pattanapong.saman@gmail.com>
2025-09-08 19:46:00 +0700

    change py 8 to 2

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,7 @@ export default function RootLayout({ children }) {
     <html lang="th">
       <body className="bg-black text-white antialiased">
         <SiteHeader />
-        <main className="mx-auto max-w-6xl px-4 md:px-6 py-8">{children}</main>
+        <main className="mx-auto max-w-6xl px-4 md:px-6 py-2">{children}</main>
       </body>
     </html>
   );
