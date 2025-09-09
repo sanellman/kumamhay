d0165c9478e103e2f25ba7c70965cd2bb490ad7f
SanellmanL <pattanapong.saman@gmail.com>
2025-09-09 09:23:40 +0700

    add analytic

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
 import SiteHeader from "./components/site-header";
 
 export const metadata = {
@@ -12,6 +13,7 @@ export default function RootLayout({ children }) {
       <body className="bg-black text-white antialiased">
         <SiteHeader />
         <main className="mx-auto max-w-6xl px-4 md:px-6 py-2">{children}</main>
+        <Analytics />
       </body>
     </html>
   );
